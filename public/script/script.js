56e931d618c4ce2b8e3015459973bc1a4856a433
Vladislav <vlad_24_9898@mail.ru>
2017-04-03 01:09:05 +0400

    upgrade validation

--- public/script/script.js
+++ public/script/script.js
@@ -11,20 +11,20 @@ var count = 0;
 function createFormGroup(){
 	count++;
 	if(this.value == "Я преподаватель"){
-		
+
 		userForms.removeChild(userForms.children[8]);
-		
+
 		var div = document.createElement('div');
-		div.className = "form-group";
+		div.className = "form-group class7";
 		div.innerHTML = `<label for="teacherkey">Уникальный идентификатор преподавателя:</label>
-				<input class="form-control" name="teacherkey" placeholder="id100" oninput="validateComments2(this)"/>`;
+				<input class="form-control" name="teacherkey" placeholder="id100" >`;
 
 		userForms.insertBefore(div, userForms.children[8]);
 
 		var div1 = document.createElement('div');
-		div1.className = "form-group";
+		div1.className = "form-group class8";
 		div1.innerHTML = `<label for="pulpit">Кафедра:</label>
-				<input class="form-control" name="pulpit"placeholder="ЭВМ" oninput="validateComments3(this)"/>`;
+				<input class="form-control" name="pulpit"placeholder="ЭВМ"/>`;
 
 		userForms.insertBefore(div1, userForms.children[9]);
 		count++;
@@ -35,12 +35,12 @@ function createFormGroup(){
 			userForms.removeChild(userForms.children[8]);
 			userForms.removeChild(userForms.children[8]);
 		}
-		
+
 		var div2 = document.createElement('div');
-		div2.className = "form-group";
+		div2.className = "form-group class6";
 		div2.innerHTML = `<label for="group">Группа(Например: ИВТ-260):</label>
-				<input class="form-control" name="group" placeholder="ИВТ-260" oninput="validateComments4(this)"/>`;
-				
+				<input class="form-control" name="group" placeholder="ИВТ-260"/>`;
+
 		userForms.insertBefore(div2, userForms.children[8]);
 		count++;
 		//console.log(count);
